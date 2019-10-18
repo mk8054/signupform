014ca607aa758bfdefb684d8e637288a189f98f0
Mohit Bhagat <mbhagat751@gmail.com>
2019-10-18 10:35:42 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'signup';
+  img = "";
+  filename = "Choose File"
+  signupform: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.signupform = this.fb.group(
+      {
+        'fname': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+        'lname': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+        'email': ['', Validators.compose([Validators.required, Validators.email])],
+        'mobile': ['', Validators.compose([Validators.required, Validators.pattern('[0-9]{10}')])],
+      }
+    )
+  }
+
+  submit() {
+    console.log(this.signupform.value);
+  }
+
+  uploadImage(img) {
+    this.filename = img[0].name;
+    console.log(img)
+    this.img = img
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
